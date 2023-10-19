888182867807ff07713030b577104d3acb0d30d1
Rowen Ellis <362862286@GoTVDSB.ca>
2023-10-19 10:12:45 -0400

    Add project files.

---
+++ Topic 5.5 new/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Topic_5._5_new
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
